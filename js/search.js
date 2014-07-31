13b3474e22cbf516ae20d7aa9be5104a5ceeb312
nayeon-factual <nayeon@factual.com>
2014-07-31 15:23:10 -0700

    Select2 Added

--- js/search.js
+++ js/search.js
@@ -1,9 +1,6 @@
 function keyPress(){
     //on enter
     if (event.keyCode == 13) {
-        $("#searchInput")
-    //on colon
-    }else if(event.keyCode == 186){
         $("#searchInput").autocomplete({
         source: Object.keys(keyTerms)
       });
