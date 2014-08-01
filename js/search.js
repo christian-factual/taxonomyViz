a41bff042ee54e886b979f41ea5997b84955bf83
nayeon-factual <nayeon@factual.com>
2014-08-01 11:38:12 -0700

    more merge conflict fix

--- js/search.js
+++ js/search.js
@@ -44,14 +44,16 @@ $(document).ready( function() {
             var fullCategory = taxonomy[id];
             var relatedTerms = getKeyTerms(fullCategory);
             relatedTerms = relatedTerms.substring(0, relatedTerms.length-2);
-            searchInputOptions+='<optgroup label="'+relatedTerms+'"><option value="'+id+'" onmouseover="onSelectionHover('+id+')">'+fullCategory+'</option></optgroup><br>';
+            searchInputOptions+='<optgroup label="'+relatedTerms+'"><option value="'+id+'" onmouseover="onSelectionHover('+id+');">'+fullCategory+'</option></optgroup><br>';
         }
         $("#searchInput").html(searchInputOptions);
     }
     
-//    function onSelectHover(value){//value = category_id "154"
-//    }
-}
+    function onSelectHover(value){//value = category_id "154"
+        highlightCategory(value);
+        console.log("highlight");
+    }
+
 
 $("#searchInput").chosen({no_results_text: "Oops, Nothing found!"});
 
