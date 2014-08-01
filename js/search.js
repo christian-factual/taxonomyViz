ad4a23168801862465d4567aec7171feb1b84dbe
nayeon-factual <nayeon@factual.com>
2014-07-31 20:26:32 -0700

    search box populated with categories list

--- js/search.js
+++ js/search.js
@@ -1,28 +1,38 @@
-var categoryOptions = [];
-for(var key in taxonomy) {
-    var value = objects[key];
+populateSearchInput();
+
+function populateSearchInput(){
+    var searchInputOptions = "";
+    for(var key in taxonomy) {
+        var value = taxonomy[key];
+        searchInputOptions+='<option value="'+key+'">'+value+'</option><br>';
+    }
+    $("#searchInput").html(searchInputOptions);
 }
 
-$(function() {
-    var data = [
-      { label: "Surf", category: "" },
-      { label: "Restaurants", category: "" },
-      { label: "Mexican", category: "" },
-      { label: "American", category: "Did you mean..." },
-      { label: "Sports", category: "Did you mean..." }
-    ];
- 
-    $("#searchInput").autocomplete({
-      delay: 0,
-      source: data
-    });
-  });
 
-function keyPress(){
-    //on enter
-//    if (event.keyCode == 13) {
-//        $("#searchInput").autocomplete({
-//        source: Object.keys(keyTerms)
-//      });
-//    }
-}
+$("#searchInput").chosen({no_results_text: "Oops, nothing found!"});
+
+
+//$(function() {
+//    var data = [
+//      { label: "Surf", category: "" },
+//      { label: "Restaurants", category: "" },
+//      { label: "Mexican", category: "" },
+//      { label: "American", category: "Did you mean..." },
+//      { label: "Sports", category: "Did you mean..." }
+//    ];
+// 
+//    $("#searchInput").autocomplete({
+//      delay: 0,
+//      source: data
+//    });
+//  });
+
+//function keyPress(){
+//    //on enter
+////    if (event.keyCode == 13) {
+////        $("#searchInput").autocomplete({
+////        source: Object.keys(keyTerms)
+////      });
+////    }
+//}
